b82196650c4bda5f35fa380f54aa2c5f46272a16
Guilherme Coutinho <guilhermecoutslc@outlook.com>
2025-06-04 12:00:27 -0300

    feat: Home Screen Creation

---
+++ lib/screens/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Menu"),
+        leading: IconButton(onPressed: (){
+          Navigator.pop(context);
+        }, icon: Icon(Icons.arrow_back_rounded )),
+        toolbarHeight: 45,
+        backgroundColor:  Color(0xffbebebe),
+      ),
+      body:
+        Center(
+          child: Column(
+            children: [
+              Icon(Icons.person, size: 90,),
+              Text("Hello Guilherme!",
+              style: TextStyle(
+                fontSize: 30
+              ),),
+            ],
+          ),
+        ),
+        backgroundColor: Color(0xffe6f877),
+
+    );
+  }
+}
